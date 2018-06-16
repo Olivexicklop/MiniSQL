f0a2368fd37684b3db021a8455c691d22d4af512
ClarissaYL <37499513+ClarissaYL@users.noreply.github.com>
2018-06-16 19:26:09 +0800

    Add files via upload

---
+++ genaral.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdio>
+
+#include "general.h"
+#include "constant.h"
+
+using namespace std;
+
+int General:: getTypeSize(short type)
+{
+	if (type == TYPE_NULL) return 0;
+	else if (type == TYPE_INT || type == TYPE_FLOAT) return 4;
+	else if (type < TYPE_CHAR) return type + 1;   //²»Ì«¶®
+
+	else
+	{
+		cerr << "ERROR: [Utils::getTypeSize] Unknown type " << type << "! " << endl;
+		cerr << "Please use type 'char', 'int' or 'float'(MiniSQL only supports these three data types)" << endl;
+		return 0;
+	}
+}
---
+++ general.h
@@ -0,0 +1,10 @@
+#ifndef _GENERAL_H_
+#define _GENERAL_H_
+
+class General {
+public:
+	static int getTypeSize(short type);
+	static char* getDataFromStr(const char* s, short type);
+	static bool dropFile(const char*);
+}
+#endif
